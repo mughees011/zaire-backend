073bfc66f9aa5ac60a70994f57ac67d167503bc0
mughees011 <mugheessiddiqui202@gmail.com>
2026-06-26 21:00:35 +0500

    Fixed websocket console errors

--- index.js
+++ index.js
@@ -1126,7 +1126,7 @@ const io = new Server(server, {
     credentials: true
   },
   maxHttpBufferSize: 1e7,
-  transports: ['polling', 'websocket'],
+  transports: ['polling'],
   allowUpgrades: true,
   pingTimeout: 60000,
   pingInterval: 25000
